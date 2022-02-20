cdd365b1e052304a95a9d0d880953386afb41a73
Katarina Spremic <spremic.katarina@gmail.com>
2022-02-20 12:50:33 +0100

    add memtable and lru config

--- lru/cache.go
+++ lru/cache.go
@@ -1,12 +1,17 @@
 package lru
 
 import (
+	"bufio"
 	"container/list"
+	"errors"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 const (
-	DEFAULT_CAPACITY = 10
+	CAPACITY = 50
 )
 
 type Pair struct {
@@ -16,25 +21,41 @@ type Pair struct {
 
 type Cache struct {
 	capacity		int
-	data			*list.List					// Lista cuva par kljuc-vrednost, najskoriji element je na kraju
-	dataMap			map[string]*list.Element	// Kljuc i pokazivac na element u listi
+	data			*list.List		// Contains Pair objects, sorted by least recently used to most recently
+	dataMap			map[string]*list.Element
 }
 
-// NewCache : Konstruktor LRU cache sa default kapacitetom
-func NewCache() *Cache {
+func NewCache() *Cache  {
 	c := new(Cache)
-	c.capacity = DEFAULT_CAPACITY
+	c.loadConfig()
 	c.dataMap = make(map[string]*list.Element)
 	c.data = list.New()
 	return c
 }
 
-// SetCapacity : Podesavanje kapaciteta
-func (cache *Cache) SetCapacity(c int) {
-	cache.capacity = c
+func (cache *Cache) loadConfig() {
+	file, err := os.OpenFile("lru/config.txt", os.O_RDONLY, 0700)
+	if errors.Is(err, os.ErrNotExist) {
+		cache.capacity = CAPACITY
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	line := scanner.Text()
+	split := strings.Split(line, "=")
+	c, err := strconv.Atoi(split[1])
+	if err == nil {
+		cache.capacity = c
+	} else {
+		// Config file format incorrect, cache capacity set to default
+		cache.capacity = CAPACITY
+	}
 }
 
-// Find : Trazenje podatka u kesu - vraca vrednost ili nil i bool da li je uspesno nadjen
+// Find : Returns value or nil and bool depending on whether the element was found by key
 func (cache *Cache) Find(key string) ([]byte, bool) {
 	listItem, found := cache.dataMap[key]
 	if found {
@@ -45,17 +66,17 @@ func (cache *Cache) Find(key string) ([]byte, bool) {
 	}
 }
 
-// Add : Dodavanje elementa u kes
 func (cache *Cache) Add(key string, value []byte) {
 	p := Pair{key, value}
 	listItem, found := cache.dataMap[p.Key]
 	if found {
+		listItem.Value = p
 		cache.data.MoveToBack(listItem)
-	} else {	// Element se ne nalazi u kesu
-		if cache.data.Len()+1 <= cache.capacity {	// Kes nije popunjen
+	} else {	// Element not in cache
+		if cache.data.Len()+1 <= cache.capacity {	// Cache not up to capacity
 			cache.data.PushBack(p)
 		} else {
-			// Kes je popunjen i uklanja se prvi element iz liste (i recnika) - onaj kome se davno pristupalo
+			// Cache at full capacity - least recent element is removed from cache
 			rem := cache.data.Front()
 			delete(cache.dataMap, rem.Value.(Pair).Key)
 			cache.data.Remove(rem)
@@ -70,6 +91,7 @@ func (cache *Cache) Check() {
 	for e := cache.data.Front(); e != nil; e = e.Next() {
 		fmt.Println(e.Value)
 	}
+	fmt.Println()
 }
 
 
--- memtable/memtable.go
+++ memtable/memtable.go
@@ -1,9 +1,14 @@
 package memtable
 
 import (
+	"bufio"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -11,6 +16,7 @@ var emptyString = ""
 
 const (
 	MAX_HEIGHT = 10
+	CAPACITY = 1000
 )
 
 type Pair struct {
@@ -27,9 +33,7 @@ type SkipList struct {
 }
 
 type Node struct {
-	// Elementi su predstavljeni pomocu cvorova
-	// Cvor cuva kljuc, vrednost, tombstone i reference na ostale elemente liste
-	// Cvor se u listi pozicionira na osnovu vrednosti kljuca
+	// Elements are represented by nodes
 	Key       string
 	Value     []byte
 	TimeStamp []byte
@@ -53,24 +57,49 @@ func (s *SkipList) NewSkipList() {
 	l := []byte(emptyString)
 	Head.newNode(&emptyString, &l, 0)
 	s.Head = &Head
-	s.MaxHeight = MAX_HEIGHT // Takodje specificirano kroz konfiguracioni fajl
-	s.height = 0             // Najveca visina koju lista trenutno poseduje
-	s.Capacity = 5
+	s.loadConfig()
+	s.height = 0
 }
 
-func (s *SkipList) SetMaxHeight(h int) {
-	s.MaxHeight = h
+func (s *SkipList) loadConfig() {
+	file, err := os.OpenFile("memtable/config.txt", os.O_RDONLY, 0700)
+	if errors.Is(err, os.ErrNotExist) {
+		s.Capacity = CAPACITY
+		s.MaxHeight = MAX_HEIGHT
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	scanner.Scan()
+	line := scanner.Text()
+	split := strings.Split(line, "=")
+	c, err := strconv.Atoi(split[1])
+	if err != nil {
+		s.Capacity = uint64(c)
+	} else {
+		// Config file format incorrect, memtable Capacity set to default
+		s.Capacity = CAPACITY
+	}
+
+	scanner.Scan()
+	line = scanner.Text()
+	split = strings.Split(line, "=")
+	m, err := strconv.Atoi(split[1])
+	if err == nil {
+		s.MaxHeight = m
+	} else {
+		// Config file format incorrect, memtable MaxHeight set to default
+		s.MaxHeight = MAX_HEIGHT
+	}
 }
 
-func (s *SkipList) SetCapacity(c uint64) {
-	s.Capacity = c
-}
-
-// Insert : Dodavanje elementa u skiplistu
+// Insert : Adding or updating element
 func (s *SkipList) Insert(key string, value []byte) *SkipList {
 	update := make([]*Node, s.MaxHeight+1)
 	current := s.Head
-	// Prolazak kroz skip listu da pronadjemo kljuc ili mesto gde treba da se ubaci
+
 	for i := s.height; i >= 0; i-- {
 		for current.Next[i] != nil && current.Next[i].Key < key {
 			current = current.Next[i]
@@ -79,7 +108,7 @@ func (s *SkipList) Insert(key string, value []byte) *SkipList {
 	}
 	current = current.Next[0]
 
-	// Element nije nadjen po kljucu i moze da se ubaci
+	// Element not found by key, can be inserted
 	if current == nil || current.Key != key {
 		newLevel := s.roll()
 		if newLevel > s.height {
@@ -93,14 +122,14 @@ func (s *SkipList) Insert(key string, value []byte) *SkipList {
 		newNode := Node{}
 		newNode.newNode(&key, &value, newLevel)
 
+		// Updating references
 		for i := 0; i <= newLevel; i++ {
-			// Azuriranje referenci
 			newNode.Next[i] = update[i].Next[i]
 			update[i].Next[i] = &newNode
 		}
 
 		s.Size += 1
-		// Novo
+
 		// If max capacity is reached we return the skiplist to be flushed on to the disk
 		if uint64(s.Size) >= s.Capacity {
 			return s
@@ -108,26 +137,24 @@ func (s *SkipList) Insert(key string, value []byte) *SkipList {
 		}
 
 	}
-	// Novo
+
+	// Element found by key, to be updated
 	if current != nil && current.Key == key {
-		fmt.Println(current.Key)
-		fmt.Println(current.Value)
-		fmt.Println(value)
 		current.Value = value
-		fmt.Println(current.Value)
-		//
+		Time := time.Now().Unix()
+		timeStamp := make([]byte, 16)
+		binary.LittleEndian.PutUint64(timeStamp, uint64(Time))
+		current.TimeStamp = timeStamp
 	}
 	return nil
 
 }
 
-// Find : Trazenje elementa u listi po vrednosti kljuca
-// Vraca vrednost koja se nalazi pod tim kljucem
+// Find : Returns value of the element found by key
 func (s *SkipList) Find(key string) []byte {
 
 	update := make([]*Node, s.MaxHeight+1)
 	current := s.Head
-	// Prolazak kroz skip listu da pronadjemo kljuc
 	for i := s.height; i >= 0; i-- {
 		for current.Next[i] != nil && current.Next[i].Key < key {
 			current = current.Next[i]
@@ -141,13 +168,11 @@ func (s *SkipList) Find(key string) []byte {
 	return nil
 }
 
-// Delete : Logicko brisanje
-// Ukoliko je element nadjen u listi pod zadatim kljucem tombstone se postavlja na true
+// Delete : Logical, if element exists by key tombstone is set to true
 func (s *SkipList) Delete(key string) bool {
 
 	update := make([]*Node, s.MaxHeight+1)
 	current := s.Head
-	// Prolazak kroz skip listu da pronadjemo kljuc
 	for i := s.height; i >= 0; i-- {
 		for current.Next[i] != nil && current.Next[i].Key < key {
 			current = current.Next[i]
@@ -162,7 +187,7 @@ func (s *SkipList) Delete(key string) bool {
 	return false
 }
 
-// ExtractData : Vraca listu referenci na parove kljuc-vrednost
+// ExtractData : Returns list of references to Pair objects
 func (s *SkipList) ExtractData() []*Pair {
 	h := s.Head
 	list := []*Pair{}
@@ -178,7 +203,7 @@ func (s *SkipList) ExtractData() []*Pair {
 }
 
 func (s *SkipList) roll() int {
-	level := 0 // alwasy start from level 0
+	level := 0 // always start from level 0
 
 	// We roll until we don't get 1 from rand function and we did not
 	// outgrow maxHeight. BUT rand can give us 0, and if that is the case
